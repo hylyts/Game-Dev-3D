1208a28b0a96de326f718e290c89e9ce523385b9
hylyts <105838217+hylyts@users.noreply.github.com>
2023-05-21 09:17:40 +0800

    Game Dev 3D

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+
+    [SerializeField] AudioSource jumpSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiInput = Input.GetAxis("Horizontal");
+        float vertiInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horiInput * moveSpeed, rb.velocity.y, vertiInput * moveSpeed);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded()) 
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        jumpSound.Play();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy Head"))
+        {
+            Destroy(collision.transform.parent.gameObject);
+            Jump();
+        }
+    }
+
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
+    }
+    
+}
